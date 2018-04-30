edbdb062b76ea2056eb40858f8c4ee2b52e7d345
anxu5829 <anxu5829@gmail.com>
2018-05-01 00:11:47 +0800

    add iron counter file

---
+++ stableMethod/ironCounter.py
@@ -0,0 +1,133 @@
+import os
+import glob
+from sklearn.externals import joblib
+from sklearn.metrics.pairwise import cosine_similarity
+import scipy.spatial as sp
+from numpy.linalg import norm
+from .utils import  *
+
+if __name__ == "__main__":
+
+    os.chdir("C:\\Users\\an\\Documents\\work\\铜片\\opencv快速入门\\chapter01 图像调整")
+    # 先进行gamma变换处理
+
+    img = cv2.imread("./sample/sample1.jpg")
+    # img = cv2.imread("copper.jpg")
+    # 添加调整图像水平的代码
+    dst = derectionCorrect(img)
+    plotCorrect = True
+    if plotCorrect:
+        plt.subplot(121)
+        plt.imshow(img)
+        plt.subplot(122)
+        plt.imshow(dst)
+    plt.show()
+    img = dst
+    hist_b = cv2.calcHist([img], [0], None, [256], [0, 256])
+    hist_g = cv2.calcHist([img], [1], None, [256], [0, 256])
+    hist_r = cv2.calcHist([img], [2], None, [256], [0, 256])
+
+    img_corrected = gamma_trans(img, 0.5)
+    cv2.imwrite('gamma_corrected_iron.png', img_corrected)
+
+    # 跳过分类器部分，直接对于铜片进行检测
+
+    imgC = cv2.imread("gamma_corrected_iron.png")
+    imgC = cv2.cvtColor(imgC, cv2.COLOR_BGR2RGB)
+    # 取出蓝色通道
+    # img  = cv2.cvtColor(imgC,cv2.COLOR_BGR2HSV)[...,0]
+    # img = cv2.imread("gamma_corrected_iron.png", 0)
+
+    b, g, r = cv2.split(imgC)
+
+    img = (g.astype(np.float32) + r.astype(np.float32)) / (b.astype(np.float32) + 10)
+    trans = True
+    if trans:
+        img = img.transpose()
+        imgC = imgC.transpose(1, 0, 2)
+
+    # 交互式的获取铜片的位置，并且进行拉伸变换
+    plt.imshow(imgC)
+    pos = plt.ginput(4)  # 依照顺序框出四个点
+    plt.close()
+    pos = np.array([[i[0], i[1]] for i in pos])
+    xstart = int(pos[:, 0].min())
+    xend = int(pos[:, 0].max())
+    ystart = int(pos[:, 1].min())
+    yend = int(pos[:, 1].max())
+
+    imgC = imgC[ystart:yend, xstart:xend]
+    img = img[ystart:yend, xstart:xend]
+
+    plt.imshow(imgC)
+    plt.show()
+
+    # 修改内容：添加了对于区域的自适应的范围选择
+    rowLength = imgC.shape[0]
+    rowStart = int(rowLength * 0.2)
+    rowEnd = int(rowLength * 0.8)
+
+    rowList = np.array(range(rowStart, rowEnd, 10))
+    np.random.shuffle(rowList)
+
+    # rowList = [750]
+    counter = []
+    lr = None
+    plotNum = 0
+    plotCounter = 0
+
+    #
+    #
+    # for startRow in rowList:
+    #     cnt = showResult(lr, startRow=startRow, rowNum=10,
+    #                  plot=True, argrelmaxOrder=40, d=3, scale=1.15
+    #                  )
+    #
+    #     print(cnt)
+    #
+    #     counter.append(cnt)
+    #
+    # print(counter)
+    #
+    # from   collections import Counter
+    #
+    # print(Counter(counter))
+    #
+    #
+    #
+    nrowList = rowList.shape[0]
+    rowLists = np.split(rowList, [
+        int(0.2 * nrowList),
+        int(0.4 * nrowList),
+        int(0.6 * nrowList),
+        int(0.8 * nrowList)
+    ])
+
+    counter = []
+    lr = None
+    plotNum = 0
+    plotCounter = 0
+
+    for rowlist in rowLists:
+        for startRow in rowlist:
+            cnt = showResult(lr, startRow=startRow, rowNum=10,
+                             plot=True, argrelmaxOrder=30, d=3, scale=1.15
+                             )
+
+            print(cnt)
+            counter.append(cnt)
+
+        # 判断counter 的计数结果，确认是否是合理的
+        from collections import Counter
+
+        cnter = dict(Counter(counter))
+        cnterResult = max(cnter, key=cnter.get)
+        prob = cnter[cnterResult] / len(counter)
+        print(prob)
+        print(cnter)
+
+        if prob > 0.8:
+            break
+
+    print(counter)
+    print(Counter(counter))
---
+++ stableMethod/utils.py
@@ -0,0 +1,357 @@
+# 查看在我们铜片上的算法的表现
+
+
+# 1 gamma 处理
+
+# gamma
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from scipy.signal import argrelmax, argrelmin
+
+
+IMAGE_NEED = 3
+
+
+# 定义Gamma矫正的函数
+def gamma_trans(img, gamma):
+    """
+    :param img: 输入的图像
+    :param gamma:  矫正图像所使用的gamma系数
+    :return:  返回一张矫正好的图像
+    """
+    # 具体做法是先归一化到1，然后gamma作为指数值求出新的像素值再还原
+    gamma_table = [np.power(x / 255.0, gamma) * 255.0 for x in range(256)]
+    gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
+
+    # 实现这个映射用的是OpenCV的查表函数
+    return cv2.LUT(img, gamma_table)
+
+
+def findSplit(b):
+    u = b.mean()
+    temp = []
+    # c = b.copy()
+    # c.sort()
+    # c = np.unique(c)
+    # #     print(c)
+    c = np.linspace(b.min(), b.max(), num=20, endpoint=False)
+    for i in c[1:]:
+        x1 = b[b < i]
+        y1 = b[b >= i]
+        m1 = x1.__len__()
+        m2 = y1.__len__()
+        temp.append(m1 * (x1.mean() - u) ** 2 + m2 * (y1.mean() - u) ** 2)
+
+    temp = np.array(temp)
+    split = c[np.argmax(temp)]
+    return split, temp
+
+
+def getLocalWidth(b, Cols, plotSplit=True):
+    # b: interval of x
+    # x : mean of some rows of image
+    # b = x[427:501]
+    # b = x[649:742]
+    split, temp = findSplit(b)
+
+    # 只选择那些split point 以上的线段只有一段或者两段的统计宽度
+    point = np.sum(np.diff(b >= split) == 1)
+    if point > 2:
+        return -1
+
+    # splitInterval : 一段内大于阈值的部分
+    splitInterval = (b > split).astype(np.int)
+    splitInterval = np.concatenate([splitInterval, [0]])
+    # x = [0,0,1,1,0,0,1,1,1,1,0]
+    begin = 0
+    end = 0
+    beginMax = 0
+    endMax = 0
+    for i in range(len(splitInterval)):
+        if splitInterval[i] == 0:
+
+            # 判断是否更新最优间隔
+            if end - begin >= endMax - beginMax:
+                # 记录当前最优的间隔：
+                beginMax = begin
+                endMax = end
+            # 重置begin,end的位置
+            begin = i
+            end = i
+        else:
+            end = i
+
+    ironWidth = endMax - beginMax
+
+    if plotSplit:
+        print(Cols, ironWidth)
+        plt.plot(temp)
+        plt.show()
+        plt.plot(b)
+        plt.axhline(split)
+        plt.show()
+
+    return ironWidth
+
+
+def searchEdge(ironSub, classifier, plot=False, bias=20):
+    """
+
+    :param ironSub: 一张传入进去的子图
+    :param classifier:  进行边缘和铜片识别的分类器
+    :param plot:  是否要将边缘识别的结果可视化出来
+    :param bias:  bias ，找到bias后向两侧稍作偏移，避免倾斜造成的误判
+    :return: 返回两个边缘点
+    """
+    edge = classifier.predict(ironSub.reshape(-1, 3)).reshape(ironSub[..., 0].shape).mean(0) > 0.5
+    edgePoints = np.argwhere(np.diff(edge) == 1).ravel()
+    try:
+        maxSub = np.argmax(np.diff(edgePoints))
+    except:
+        assert 1 != 2
+    edgeP = [edgePoints[maxSub], edgePoints[maxSub + 1]]
+    edgeP = [edgeP[0] - bias, edgeP[1] + bias]
+    if plot:
+        figure = plt.figure(figsize=(15, 15))
+        plt.imshow(ironSub)
+
+        # 注意：实际上去取范围不会取的这么大
+        plt.axvline(edgeP[0], color='r', linewidth=4)
+        plt.axvline(edgeP[1], color='r', linewidth=4)
+    # 考虑到倾斜，加入bias
+    return edgeP
+
+
+def showResult(classifier,
+               startRow=500, rowNum=100,
+               d=4,
+               argrelmaxOrder=10, numOfStopping=20, scale=1.3
+               , plot=True, plotRowNum=40, coords=False, useMax=True
+               ):
+    """
+
+    :param classifier: 边缘分类器
+    :param startRow:  本次计数所使用的开始的位置
+    :param rowNum:  本次计数使用的行数
+    :param d:  进行差分的间隔
+    :param argrelmaxOrder:  初始化寻找局部最大值的参数
+    :param numOfStopping:  迭代次数
+    :param scale:  放缩比例
+    :param plot: 是否进行画图
+    :param plotRowNum:  plot参数
+    :return: 返回一系列的坐标
+    """
+    # 先找到边缘
+    # edge = searchEdge(imgC[startRow:(startRow + rowNum), :], classifier, plot=True, bias=5)
+    # print(edge)
+
+    countCol = slice(0, -1)
+    # countCol = slice(edge[0], edge[1])
+
+    # 局部计数
+    # cv2.imwrite("img_600_900.png",img[400:410,600:900])
+
+    # x = img[startRow:(startRow + rowNum), countCol].mean(0)
+
+    '''
+    testing code here
+    '''
+    # lr = joblib.load("logisticForIronPad.model")
+    # lr.predict(imgC[startRow:(startRow + rowNum), countCol].mean(0))
+    # # plt.imshow(imgC[startRow:(startRow + 50), countCol])
+    # predictAsPad = lr.predict(imgC[startRow:(startRow + rowNum), countCol].mean(0))
+    # # plt.plot(predictAsPad*50)
+    imgc = img[:, countCol].copy()
+    # imgc[:,predictAsPad == 1] = 0
+    x = imgc[startRow:(startRow + rowNum), countCol].mean(0)
+
+    '''
+    plt.imshow(imgc)
+    x = imgc[startRow:(startRow + rowNum), countCol].mean(0)
+    plt.plot(-x*500)
+
+    '''
+
+    '''
+    testing code here
+    '''
+
+    # cv2.imwrite("img_" + str(startRow) + "_100_2000.png", img[startRow:(startRow + rowNum), countCol])
+
+    diff = x[d:] - x[:-d]
+
+    '''
+    plt.imshow(imgc)
+    plt.plot(-diff*500-300)
+
+    '''
+    np.random.seed(10)
+    diff = diff + np.random.random(diff.shape[0]) * 0.0001
+    # plt.imshow( img[startRow:(startRow +40), countCol])
+    # plt.plot(diff)
+
+    currentCount = 0
+    flag = 0
+    xcoords = []
+    stopWhile = 0
+
+    while (True):
+        stopWhile += 1
+        xcoords = argrelmax(diff, order=argrelmaxOrder)[0]
+        diffXcocrds = np.diff(xcoords)
+        low = np.percentile(diffXcocrds, 15)
+        high = np.percentile(diffXcocrds, 85)
+        #         print(diffXcocrds)
+        mean = (diffXcocrds[(diffXcocrds >= low) * (diffXcocrds <= high)]).mean()
+        try:
+            argrelmaxOrder = int(scale * mean / 2)
+        except:
+            print("get error")
+            assert 1 != 2
+
+        if currentCount == xcoords.__len__():
+            flag += 1
+        else:
+            currentCount = xcoords.__len__()
+        if flag >= numOfStopping or stopWhile > 30:
+            break
+
+    if coords:
+        return xcoords
+    else:
+
+        '''
+        查看最原始的分割结果对应的图片：
+            fig = plt.figure(figsize=(20,3))
+            plt.title("counter : " + str(counter))
+            plt.imshow(imgC[startRow:(startRow + plotRowNum), countCol])
+            plt.plot(diff*100)
+            for xc in xcoords:
+                plt.axvline(x=xc, color='r',linewidth = 4)
+            print(plotNum)
+            plt.show()
+
+
+       '''
+
+        interval = np.diff(xcoords)
+        firstOutpoint = interval.mean() - interval.std() * 3
+        while True:
+            interval = np.diff(xcoords)
+            correct2 = interval[interval < firstOutpoint]
+            for i in correct2:
+                locate = np.argwhere(interval == i).ravel()[0]
+                # 去除不需要的间隔过小的点
+                xcoords = np.delete(xcoords, locate + 1)
+            if correct2.shape[0] == 0:
+                break
+
+        # 获得修正后的xcoords，消除了异常的小间隔
+        # 再获取铜片宽度
+        ironWidthList = []
+        for j in range(xcoords.__len__() - 1):
+            # for j in [17]:
+            # b : 拿到了一个间隔中的所有的原始的像素点
+            b = x[xcoords[j]: xcoords[j + 1]]
+
+            '''
+            找到b对应的那段图像是什么样的
+
+            plt.imshow( imgC[startRow:(startRow+50),xcoords[j]: xcoords[j + 1]])
+            plt.plot(b*50)
+
+           '''
+            ironWidthList.append(getLocalWidth(b, (xcoords[j], xcoords[j + 1]), plotSplit=False))
+
+        ironWidthList = np.array(ironWidthList)
+        ironWidthList = ironWidthList[ironWidthList != -1]
+        ironWidthList.sort()
+        ironWidth = np.median(ironWidthList)
+
+        counter = xcoords.__len__()
+
+        correct = interval[interval > interval.mean() + interval.std() * 2]
+
+        # 利用铜片宽度，消除异常：
+        if correct.shape[0] != 0:
+            for index in range(len(correct)):
+                # 定位:
+                loc = np.argwhere(interval == correct[index]).ravel()[0]
+                doubtInterval = x[xcoords[loc]:xcoords[loc + 1]]
+
+                # plt.imshow(imgC[startRow :(startRow + 50) , xcoords[loc]:xcoords[loc + 1]])
+                # plt.plot(doubtInterval)
+
+                split, _ = findSplit(doubtInterval)
+
+                # 获得大于split的部分：
+                correct3 = round((doubtInterval >= split).sum() / ironWidth) - 1
+                counter += correct3 if correct3 > 0 else 0
+                # plt.plot(doubtInterval)
+                # plt.imshow(imgC[startRow:(startRow+plotRowNum),xcoords[loc]:xcoords[loc + 1],:])
+        # 考虑是否进行最终结果的作图
+        if plot:
+            global plotNum, plotCounter
+
+            fig = plt.figure(figsize=(20, 20))
+
+            plt.title('startRow ' + str(startRow) +
+                      ' rowNum ' + str(rowNum) +
+                      ' d ' + str(d) +
+                      ' scale ' + str(scale) +
+                      ' argrelmaxOrder' + str(argrelmaxOrder) +
+                      ' correct counter ' + str(counter) +
+                      ' counter ' + str(xcoords.__len__())
+                      )
+
+            plt.plot(diff)
+            for xc in xcoords:
+                plt.axvline(x=xc, color='r')
+            plt.show()
+
+            fig = plt.figure(figsize=(20, 3))
+            plt.title("counter : " + str(counter))
+            plt.imshow(imgC[startRow:(startRow + plotRowNum), countCol])
+            for xc in xcoords:
+                plt.axvline(x=xc, color='r', linewidth=4)
+            print(plotNum)
+            plt.show()
+
+        return counter
+
+
+def derectionCorrect(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.bilateralFilter(img, 15, 75, 75)
+    edges = cv2.Canny(blur, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 300)
+    savePlot = False
+
+    if savePlot:
+        imgForSave = img.copy()
+        for line in lines:
+            line = line.ravel()
+            rho = line[0]
+            theta = line[1]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 2000 * (-b))
+            y1 = int(y0 + 2000 * (a))
+            x2 = int(x0 - 2000 * (-b))
+            y2 = int(y0 - 2000 * (a))
+            cv2.line(imgForSave, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.imwrite("imgAfterCorrect.jpg", imgForSave)
+
+    theta = lines.reshape(-1, 2)[:, 1]
+    theta_high = np.percentile(theta, 85)
+    theta_low = np.percentile(theta, 15)
+    thetaMean = theta[(theta <= theta_high) * (theta >= theta_low)].mean()
+
+    rows, cols, _ = img.shape
+    # M = cv2.getRotationMatrix2D((cols / 2, rows / 2),    - thetaMean  , 1.1)
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), thetaMean * 180 / 3.1415926 - 90, 1.1)
+    dst = cv2.warpAffine(img, M, (cols, rows))
+    return dst
